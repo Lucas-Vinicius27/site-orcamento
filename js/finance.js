0f8e2f2a8566cf12df06287d85c66291fa28947e
Lucas Vinicius <lucas_vfsantos@hotmail.com>
2021-01-28 19:33:41 -0300

    Update finance.js

--- js/finance.js
+++ js/finance.js
@@ -6,3 +6,30 @@ const modal = {
         document.querySelector('.modal-overlay').classList.remove('active');
     }
 };
+
+const transaction = [
+    {
+        id: 1,
+        description: 'Luz',
+        amount: -50000,
+        date: '23/01/2021',
+    },
+    {
+        id: 2,
+        description: 'Criação Website',
+        amount: 500000,
+        date: '23/01/2021',
+    },
+    {
+        id: 3,
+        description: 'Internet',
+        amount: -20000,
+        date: '23/01/2021',
+    }
+];
+
+const balance = {
+    incomes() { },
+    expenses() { },
+    total() { }
+};
